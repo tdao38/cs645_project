ce7c8779a504f9893bd959a0b2163724afc89864
tdao38 <tdao38@massmutual.com>
2021-04-15 10:47:53 -0400

    added docstring and comments

--- src/segmentation.py
+++ src/segmentation.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 
 def mapping(index_data):
+    """
+    STEP 1:
+    Calculate the length of the abnormal class.
+    Sampling size of reference class is calculated by doubling the length of the abnormal class.
+    :param index_data: the original ground truth data frame
+    :return: index_data: with added tsa, tsa, normal_start, normal_end columns
+    """
     # find the refernced index begining
     index_data['tsa'] = index_data['end'] - index_data['start'] + 1
     index_data['tsr'] = index_data['tsa']*2
@@ -11,6 +18,12 @@ def mapping(index_data):
     return index_data
 
 def calculate_class_entropy(index_data):
+    """
+    STEP 2:
+    Calculate the class entropy based on the sampled reference class
+    :param index_data: index_data after STEP 1
+    :return: index_data: with added h_class column that represents the class entropy
+    """
     # calculate the class entropy & add a new column called class_entropy
     pa = index_data['tsa']/(index_data['tsa']+index_data['tsr'])
     pr = index_data['tsr']/(index_data['tsa']+index_data['tsr'])
@@ -20,6 +33,15 @@ def calculate_class_entropy(index_data):
     return index_data
 
 def select_segment(data, index_data):
+    """
+    STEP 3:
+    Calculate the length of the abnormal class.
+    Sampling size of reference class is calculated by doubling the length of the abnormal class.
+    :param index_data: index_data after STEP 2
+    :param data: the cleaned dataset that contains all features
+    :return: filtered_data: a list of 6 data frames, representing the original data seperated by the segment
+    they belong to calculated from STEP 1
+    """
     # create a list of numpy array that contain filtered segments
     filtered_data = []
     for i in range(len(index_data)):
@@ -31,6 +53,12 @@ def select_segment(data, index_data):
     return filtered_data
 
 def calculate_segment_entropy(filtered_data):
+    """
+    STEP 4:
+    Calculate the segmentation entropy for each feature in each of the 6 dataframes in filtered_data
+    :param filtered_data: list of 6 data frames from STEP 3
+    :return: results_df: 6x19 dataframe, storing segmentation entropy for each feature in each of the 6 dataframes
+    """
 
     results_matrix = np.zeros((len(filtered_data), len(filtered_data[0].columns)-3))
     results_df = pd.DataFrame(data=results_matrix, columns=filtered_data[0].columns[1:-2])
@@ -60,19 +88,33 @@ def calculate_segment_entropy(filtered_data):
     return results_df
 
 def calculate_segment_penalty(df_feature):
+    """
+    USED IN STEP 4:
+    Calculate the penalized segmentation entropy
+    :param df_feature: dataframe containing one feature, generated in the for loop in STEP 4
+    :return: h_segment_penalty_all: a number - segment penalty for that particular feature
+    """
 
+    # Loop through the unique values in df_feature
     unique_vals = pd.unique(df_feature[df_feature.columns[0]])
     h_segment_penalty_all = 0
 
+    # Calculate the segment penalty for each unique value
     for i in range(len(unique_vals)):
         df_filter = df_feature[df_feature[df_feature.columns[0]] == unique_vals[i]]
+        # Only calculate the segment penalty if the unique values contain both label (0, 1)
         if len(df_filter.label.unique()) > 1:
             a = sum(df_filter['label'] == 1)
             n = len(df_filter) - a
+            # If number of abnormal labels = normal labels => number of segment is twice the length
+            # For example: N A N A => 2*2=4 segment => penalty = 4*1/4*log(4) = log(4)
             if a == n:
                 number_of_segments = 2*a
                 h_segment_penalty = np.log(number_of_segments)
                 h_segment_penalty_all += h_segment_penalty
+            # If number of abnormal labels != normal labels => number of segment is twice the smaller length + 1
+            # For example: N A N A N N N N => 2*2+1=5 segment => penalty = 4*1/6*log(6) + 2/6*log(6/2)
+            # len_mix_segment = 4, len_pure_segment = 1
             else:
                 number_of_segments = min(a, n)*2 + 1
                 len_mixed_segment = number_of_segments - 1
