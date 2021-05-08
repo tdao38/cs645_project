356aa3a7f1073a3fa48c0eda47f45790d708fbf8
Trang Tran <ttran38@massmutual.com>
2021-05-08 02:00:51 -0400

    Add utils functions for extension

---
+++ src/extensions_utils.py
@@ -0,0 +1,25 @@
+import tensorflow.keras.backend as K
+from pathlib import Path
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+# Helper function for Keras Neural Networks
+# The codes were from an issue posted to the Keras team https://github.com/keras-team/autokeras/issues/1257
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
